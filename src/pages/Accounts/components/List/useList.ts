e68dd0df0f1557f5c50caeb783c9e207cdf1a64c
Liam Marcassa <liam@gamesheetinc.com>
2022-06-26 14:48:28 -0400

    Fix copy to clipboard not working

--- src/pages/Accounts/components/List/useList.ts
+++ src/pages/Accounts/components/List/useList.ts
@@ -2,18 +2,23 @@ import { useRecoilValue } from "recoil"
 import { calcPassword } from "../../../../crypto/encrypt";
 import { masterPasswordState } from "../../../../state"
 import { Account } from "../../../../types"
-
-
+import { isMobile } from "../../../isMobile";
 
 export const useList = () => {
     const masterPassword = useRecoilValue(masterPasswordState);
+
     const generatePassword = async (acc: Account) => {
-        navigator.clipboard.write([ new ClipboardItem({
-            'text/plain': calcPassword({
-                profile: acc,
-                masterPassword
-            })
-        })])
+        if (isMobile.iOS()) {
+            navigator.clipboard.write([ new ClipboardItem({
+                'text/plain': calcPassword({
+                    profile: acc,
+                    masterPassword
+                })
+            })])
+        } else {
+            const password = await calcPassword({ profile: acc, masterPassword });
+            navigator.clipboard.writeText(password);
+        }
     }
 
     return {
